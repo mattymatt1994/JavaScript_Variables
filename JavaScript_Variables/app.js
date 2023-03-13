524e666fc2631cacffd522299bd6ad8ad42af123
mattymatt1994 <121513907+mattymatt1994@users.noreply.github.com>
2023-03-13 15:39:44 -0400

    Add files via upload

---
+++ JavaScript_Variables/app.js
@@ -0,0 +1,34 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName = "Matt";
+let lastName = "Jordan"; 
+var age = 28;
+
+
+// Exercise 2
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+// Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let bookmark = 456;
+let selectedSize = "M";
+
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
